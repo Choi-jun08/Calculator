229c6ec4df2c3001b5d7bb71d9cf2b6db7e9f387
Choi <chltjdw@naver.com>
2024-11-18 15:45:44 +0900

    Initial Commit

---
+++ Ch3PracticeTests/Ch3PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ch3PracticeTests.swift
+//  Ch3PracticeTests
+//
+//  Created by t2023-m0072 on 11/18/24.
+//
+
+import Testing
+@testable import Ch3Practice
+
+struct Ch3PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
